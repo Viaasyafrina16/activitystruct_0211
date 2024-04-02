b05445bfa2261b78471cf119729f1702fc8bf0ea
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-02 15:31:10 +0700

    mengubah cin menjadi cout

--- praktikum5/praktikum5.cpp
+++ praktikum5/praktikum5.cpp
@@ -19,8 +19,11 @@ int main() {
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa : ";
 	cin >> mhs.nama;
+	
 	cout << "Alamat Mahasiswa : ";
-	cin >> mhs.alamat;
+	cout << "\t Nama Desa : "
+    cin >>
+	
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
 
