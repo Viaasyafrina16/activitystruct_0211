5c532f49ded76b4e4020d3b5e7fc47118a952110
Viaasyafrina16 <syafrinavia@gmail.com>
2024-04-02 15:41:14 +0700

    menambahkan cout mhs.alamat.kota

--- praktikum5/praktikum5.cpp
+++ praktikum5/praktikum5.cpp
@@ -34,6 +34,7 @@ int main() {
 	cout << "\n Nama : " << mhs.nama;
 	cout << "\n Alamat : " ;
 	cout << "\n Desa : " << mhs.alamat.desa;
+	cout << "\n Kota : " << mhs.alamat.kota;
 	cout << "\n Umur : " << mhs.umur;
 
 }
